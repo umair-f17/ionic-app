5b6795500a836067c2472e33c4cb2be68508ed8d
umairf17iiui@gmail.com <umairf17iiui@gmail.com>
2023-08-30 12:13:41 +0500

    changeed artificat commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.myApp.app',
   appName: 'myApp',
   webDir: 'www',
   server: {
